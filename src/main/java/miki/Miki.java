cdbf751763f468d72cd40a07984690aef83a2dac
pastchum <patrick.navin.thomas123@gmail.com>
2025-01-23 20:36:53 +0800

    changed name

---
+++ src/main/java/miki/Miki.java
@@ -0,0 +1,26 @@
+package miki;
+
+import java.util.Scanner;
+
+public class Miki {
+    public static void main(String[] args) {
+        String logo = "      __  _________ __ ____\n" + //
+                "     /  |/  /  _/ //_//  _/\n" + //
+                "    / /|_/ // // ,<   / /  \n" + //
+                "   / /  / // // /| |_/ /   \n" + //
+                "  /_/  /_/___/_/ |_/___/   \n" + //
+                "                           \n";
+
+        System.out.println("____________________________________________________________\n" + //
+                "Hello from \n" + logo);
+
+        while (true) {
+            System.out.println("____________________________________________________________\n");
+
+            System.out.println("Bye. See you again. \n");
+
+            System.out.println("____________________________________________________________");
+            break;
+        }
+    }
+}
