f7c78e14c76bd652ff7d13c04d5f8ff10c15111c
pastchum <patrick.navin.thomas123@gmail.com>
2025-02-22 23:41:39 +0800

    Fix Parse Storage bug

--- src/main/java/miki/task/Task.java
+++ src/main/java/miki/task/Task.java
@@ -43,17 +43,23 @@ public abstract class Task {
             if (tokens[0].equals("D")) {
                 String description = tokens[2];
                 LocalDateTime deadline = LocalDateTime.parse(tokens[4]);
-                String[] tags = tokens[3].split(" ");
+                String[] tags = tokens.length > 3 
+                    ? tokens[3].split(" ") 
+                    : new String[0];
                 task = new Deadline(description, deadline, tags);
             } else if (tokens[0].equals("E")) {
                 String description = tokens[2];
                 LocalDateTime start = LocalDateTime.parse(tokens[4]);
                 LocalDateTime end = LocalDateTime.parse(tokens[5]);
-                String[] tags = tokens[3].split(" ");
+                String[] tags = tokens.length > 3 
+                    ? tokens[3].split(" ") 
+                    : new String[0];
                 task = new Event(description, start, end, tags);
             } else {
                 String description = tokens[2];
-                String[] tags = tokens[3].split(" ");
+                String[] tags = tokens.length > 3 
+                    ? tokens[3].split(" ") 
+                    : new String[0];
                 task = new ToDo(description, tags);
             }
             if (tokens[1].equals("1") && task != null) {
